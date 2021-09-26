6271c1679813574f70e938f25fdaac2996f4ae95
Vinaybannur <91072399+Vinaybannur@users.noreply.github.com>
2021-09-26 14:52:57 +0530

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,16 +1,20 @@
 //anonymous function 
 let arr1 = [2, 4, 6, 8];
 const rotateRight= function (arr){
+    for(let i=1;i<arr.length;i++){
     let last = arr.pop();// pop() ---> removes last element
     arr.unshift(last);// unshift() --> adds one or more elements
     console.log (arr);
+    }
 }
 rotateRight(arr1);
 
 //IIFE function
  (function(arr){
+     for(let i=1;i<arr.length;i++){
     let last = arr.pop();// pop() ---> removes last element
     arr.unshift(last);// unshift() --> adds one or more elements
     console.log (arr);
+     }
  })(arr1);
 
